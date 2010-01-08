a6eaef524b2e7affd299acce25d775e830bd99cc
Widi Harsojo <wharsojo@gmail.com>
2010-01-08 15:17:26 +0700

    ready to user now...

--- js/blogspot.ready.js
+++ js/blogspot.ready.js
@@ -2,8 +2,7 @@ $(function(){
   var obj= $('pre.eval');
   if( obj,length >0){
      var  txt= obj.html().replace(/<br>/gi,'');
-     alert(txt);
-      eval(txt);
+     eval(txt);
   }
   SyntaxHighlighter.config.bloggerMode = true;
   SyntaxHighlighter.all();
