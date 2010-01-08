3784feb9f17afe2634dbb0341adb1627a7c72788
Widi Harsojo <wharsojo@gmail.com>
2010-01-08 17:45:02 +0700

    update again

--- js/blogspot.ready.js
+++ js/blogspot.ready.js
@@ -1,7 +1,7 @@
 $(function(){
   var obj= $('pre.eval');
   if( obj,length >0){
-     var  txt= obj.html().replace(/<br>/gi,'');
+     var  txt= obj.html().replace(/<br>/gi,'').replace(/<br \/>/gi,'');
      eval(txt);
   }
   SyntaxHighlighter.config.bloggerMode = true;
