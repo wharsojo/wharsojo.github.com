c4b9b2edc23c7a55e9f38a947d3fe43b0f69b615
Widi Harsojo <wharsojo@gmail.com>
2010-01-08 17:48:52 +0700

    update again...

--- js/blogspot.ready.js
+++ js/blogspot.ready.js
@@ -2,6 +2,7 @@ $(function(){
   var obj= $('pre.eval');
   if( obj,length >0){
      var  txt= obj.html().replace(/<br>/gi,'').replace(/<br \/>/gi,'');
+     alert(txt);
      eval(txt);
   }
   SyntaxHighlighter.config.bloggerMode = true;
