71d46f7a8753be1c3a8ad34af362e23c99ef233c
Widi Harsojo <wharsojo@gmail.com>
2010-01-09 20:27:13 +0700

    Update for adding css

--- js/blogspot.ready.js
+++ js/blogspot.ready.js
@@ -1,4 +1,11 @@
 $(function(){
+  var obj= $('pre.css');
+  obj.each(function(){
+     var  css= $(this);
+     var  txt= css.html().replace(/<br>/gi,'').replace(/<br \/>/gi,'');
+     css.parent().prepend(txt);
+  })
+  
   var obj= $('pre.eval');
   obj.each(function(){
      var  txt= $(this).html().replace(/<br>/gi,'').replace(/<br \/>/gi,'');
