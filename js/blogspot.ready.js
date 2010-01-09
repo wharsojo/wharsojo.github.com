5060b1ac67320baf883f1823391adcd5cc36a6b3
Widi Harsojo <wharsojo@gmail.com>
2010-01-10 06:30:51 +0700

    Update blogspot ready

--- js/blogspot.ready.js
+++ js/blogspot.ready.js
@@ -1,16 +1,22 @@
 $(function(){
+  var obj= $('pre.eval');
+  obj.each(function(){
+     eval($(this).html().replace(/<br>/gi,''));
+  });
+  
+  var obj= $('div.demo');
+  obj.each(function(){
+     var  dmo= $(this);
+     dmo.html(dmo.html().replace(/<br>/gi,''));
+  });
+  
   var obj= $('pre.css');
   obj.each(function(){
      var  css= $(this);
-     var  txt= css.html().replace(/<br>/gi,'').replace(/<br \/>/gi,'');
+     var  txt= css.html().replace(/<br>/gi,'');
      css.parent().prepend('<style>'+txt+'</style>');
-  })
+  });
   
-  var obj= $('pre.eval');
-  obj.each(function(){
-     var  txt= $(this).html().replace(/<br>/gi,'').replace(/<br \/>/gi,'');
-     eval(txt);
-  })
   SyntaxHighlighter.config.bloggerMode = true;
   SyntaxHighlighter.all();
 });
